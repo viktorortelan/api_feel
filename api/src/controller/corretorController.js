ab02b17a8db01a96458c32caaf1af9da8daf7d05
viktorortelan <jvortelan@gmail.com>
2024-10-07 15:05:33 -0300

    update tela corretor

--- api/src/controller/corretorController.js
+++ api/src/controller/corretorController.js
@@ -1,4 +1,4 @@
-import { buscarCorretor, totalCorretor } from "../repository/corretorRepository.js";
+import { buscarCorretor, totalCorretor, updateCorretor} from "../repository/corretorRepository.js";
 import {Router} from 'express';
 
 const endpoint = Router();
@@ -12,6 +12,26 @@ endpoint.get('/total/corretor', async (req, resp) => {
     let registro = await totalCorretor();
     resp.send(registro);
 });
+
+endpoint.put('/atualizar/corretor/:nome/:email/:senha/:id', async (req, resp) => {
+    try {
+        const { nome, email, senha, id } = req.params;
+
+        let registro = await updateCorretor(nome, email, senha, id);
+        
+        if (registro > 0) {
+    
+            resp.status(200).send({ message: 'Corretor atualizado com sucesso!' });
+        } else {
+    
+            resp.status(404).send({ message: 'Corretor não encontrado.' });
+        }
+    } catch (error) {
+        console.error(error);
+        resp.status(500).send({ message: 'Erro ao atualizar corretor.' });
+    }
+});
+
 
 
 export default endpoint;
--- api/src/repository/corretorRepository.js
+++ api/src/repository/corretorRepository.js
@@ -20,3 +20,23 @@ export async function totalCorretor() {
     let fim = registro[0];
     return fim[0];
 }
+
+export async function updateCorretor(nome, email, senha, id) {
+    let comando = `
+        UPDATE tb_corretores
+        SET nm_adm = ?,
+            ds_email = ?,
+            ds_senha = ?
+        WHERE id_corretor = ?
+    `;
+    
+    try {
+        let [registro] = await database.query(comando, [nome, email, senha, id]);
+        
+        return registro.affectedRows;
+    } catch (error) {
+      
+        console.error('Erro ao atualizar corretor:', error);
+        throw error; 
+    }
+}
